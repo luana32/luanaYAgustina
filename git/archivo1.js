6ab8d01abb51063a9482b8fe58392d1b328a08b1
AgustinaCastillo19 <agustinacastillo919@gmail.com>
2023-10-11 19:38:31 -0300

    agregue un array en archivo1

--- git/archivo1.js
+++ git/archivo1.js
@@ -1,3 +1,8 @@
 const fruits = new Array("Apple", "Banana");
 console.log(fruits.length); // 2
 console.log(fruits[0]); // "Apple"
+
+let otrasFrutas = ['frutillas', 'peras', 'uvas'];
+let [ , fruta2 , fruta3]= otrasFrutas;
+
+console.log(fruta2);
